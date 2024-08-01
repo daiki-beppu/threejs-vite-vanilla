be7ede058ae3b8139b35c47c91eaf30e1a2c270e
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-01 19:43:19 +0900

    add: 表示するオブジェクトを管理するクラスを追加

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,8 +1,28 @@
+import * as THREE from 'three';
 import { Experience } from '../Experience';
 
 export class World {
   constructor() {
     this.experinece = new Experience();
     this.scene = this.experinece.scene;
+    this.resources = this.experinece.resources;
+    this.debug = this.experinece.debug;
+
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder('test');
+    }
+
+    // テストメッシュ
+    const testMesh = new THREE.Mesh(
+      new THREE.BoxGeometry(3, 3, 3),
+      new THREE.MeshBasicMaterial({
+        color: 'orange',
+      })
+    );
+    if (this.debug.active) {
+    }
+
+    this.scene.add(testMesh);
   }
+  update() {}
 }
