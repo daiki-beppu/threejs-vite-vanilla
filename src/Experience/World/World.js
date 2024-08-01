23cd34f0e15fea122bbd0d68053c741516c2f39b
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-01 22:27:36 +0900

    add: テストメッシュの設定を追加
    - デフォルトの色を変更
    - ワイヤーフレームの追加

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,27 +1,28 @@
 import * as THREE from 'three';
 
 export class World {
-  constructor(experience) {
-    this.experinece = experience;
-    this.scene = this.experinece.scene;
-    this.resources = this.experinece.resources;
-    this.debug = this.experinece.debug;
+	constructor(experience) {
+		this.experinece = experience;
+		this.scene = this.experinece.scene;
+		this.resources = this.experinece.resources;
+		this.debug = this.experinece.debug;
 
-    if (this.debug.active) {
-      this.debugFolder = this.debug.ui.addFolder('test');
-    }
+		if (this.debug.active) {
+			this.debugFolder = this.debug.ui.addFolder('test');
+		}
 
-    // テストメッシュ
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(3, 3, 3),
-      new THREE.MeshBasicMaterial({
-        color: 'orange',
-      }),
-    );
-    if (this.debug.active) {
-    }
+		// テストメッシュ
+		const testMesh = new THREE.Mesh(
+			new THREE.BoxGeometry(3, 3, 3),
+			new THREE.MeshBasicMaterial({
+				color: '#FFA758',
+				wireframe: true,
+			}),
+		);
+		if (this.debug.active) {
+		}
 
-    this.scene.add(testMesh);
-  }
-  update() {}
+		this.scene.add(testMesh);
+	}
+	update() {}
 }
