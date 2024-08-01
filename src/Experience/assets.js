85509a9cfeade5b5edb9df3300fb1a46d5a55e7f
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-01 19:41:14 +0900

    add: アセットの管理をするクラスを追加

---
+++ src/Experience/assets.js
@@ -0,0 +1,22 @@
+export const assets = [
+  {
+    name: '',
+    type: 'gltfmodel',
+    path: [],
+  },
+  {
+    name: '',
+    type: 'texture',
+    path: [],
+  },
+  {
+    name: '',
+    type: 'cubeTexture',
+    path: [],
+  },
+  {
+    name: '',
+    type: 'font',
+    path: [],
+  },
+];
