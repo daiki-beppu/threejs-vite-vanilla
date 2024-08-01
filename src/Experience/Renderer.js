7bba3b4b89ed21b8c235550c241c91ded5ce7bb3
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-01 22:28:55 +0900

    change: トーンマッピングの設定と背景色の設定を削除

--- src/Experience/Renderer.js
+++ src/Experience/Renderer.js
@@ -1,36 +1,33 @@
 import * as THREE from 'three';
 
 export class Renderer {
-	constructor(experience) {
-		this.experience = experience;
-		this.canvas = this.experience.canvas;
-		this.sizes = this.experience.sizes;
-		this.scene = this.experience.scene;
-		this.camera = this.experience.camera;
+  constructor(experience) {
+    this.experience = experience;
+    this.canvas = this.experience.canvas;
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.camera = this.experience.camera;
 
-		this.setInstance();
-	}
+    this.setInstance();
+  }
 
-	setInstance() {
-		this.instance = new THREE.WebGLRenderer({
-			canvas: this.canvas,
-			antialias: true,
-		});
-		this.instance.toneMapping = THREE.CineonToneMapping;
-		this.instance.toneMappingExposure = 1.75;
-		this.instance.shadowMap.enabled = true;
-		this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
-		this.instance.setClearColor('#211d20');
-		this.instance.setSize(this.sizes.width, this.sizes.height);
-		this.instance.setPixelRatio(this.sizes.devicePixelRatio);
-	}
+  setInstance() {
+    this.instance = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+    this.instance.shadowMap.enabled = true;
+    this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.instance.setSize(this.sizes.width, this.sizes.height);
+    this.instance.setPixelRatio(this.sizes.devicePixelRatio);
+  }
 
-	resize() {
-		this.instance.setSize(this.sizes.width, this.sizes.height);
-		this.instance.setPixelRatio(this.sizes.devicePixelRatio);
-	}
+  resize() {
+    this.instance.setSize(this.sizes.width, this.sizes.height);
+    this.instance.setPixelRatio(this.sizes.devicePixelRatio);
+  }
 
-	update() {
-		this.instance.render(this.scene, this.camera.instance);
-	}
+  update() {
+    this.instance.render(this.scene, this.camera.instance);
+  }
 }
