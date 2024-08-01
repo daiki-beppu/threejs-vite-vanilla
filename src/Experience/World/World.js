7cf2bacf0c71b36bb33dac74c4ccc37bc1e7e37c
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-01 23:14:44 +0900

    fix: 誤字の修正

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,28 +1,28 @@
 import * as THREE from 'three';
 
 export class World {
-	constructor(experience) {
-		this.experinece = experience;
-		this.scene = this.experinece.scene;
-		this.resources = this.experinece.resources;
-		this.debug = this.experinece.debug;
+  constructor(experience) {
+    this.experience = experience;
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.debug = this.experience.debug;
 
-		if (this.debug.active) {
-			this.debugFolder = this.debug.ui.addFolder('test');
-		}
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder('test');
+    }
 
-		// テストメッシュ
-		const testMesh = new THREE.Mesh(
-			new THREE.BoxGeometry(3, 3, 3),
-			new THREE.MeshBasicMaterial({
-				color: '#FFA758',
-				wireframe: true,
-			}),
-		);
-		if (this.debug.active) {
-		}
+    // テストメッシュ
+    const testMesh = new THREE.Mesh(
+      new THREE.BoxGeometry(3, 3, 3),
+      new THREE.MeshBasicMaterial({
+        color: '#FFA758',
+        wireframe: true,
+      }),
+    );
+    if (this.debug.active) {
+    }
 
-		this.scene.add(testMesh);
-	}
-	update() {}
+    this.scene.add(testMesh);
+  }
+  update() {}
 }
