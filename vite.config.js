5845de06360ccc9daf15e771a85ac7d037db2fb9
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-08-17 14:09:43 +0900

    ✨ シェーダーファイルとインポートに必要なプラグインをを追加

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import glsl from 'vite-plugin-glsl';
 import restart from 'vite-plugin-restart';
 
 export default {
   root: 'src/', // Sources files (typically where index.html is)
-  publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+  publicDir: '../assets/', // Path from "root" to static assets (files that are served as they are)
   server: {
     host: true, // Open to local network and display URL
     open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
@@ -13,6 +14,7 @@ export default {
     sourcemap: true, // Add sourcemap
   },
   plugins: [
-    restart({ restart: ['../static/**'] }), // Restart server on static file change
+    restart({ restart: ['../assets/**'] }), // Restart server on static file change
+    glsl(),
   ],
 };
